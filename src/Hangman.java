5191648e21585594e75156502d9fd1d87973d017
jonsmacbookair <jonathan.morrisseyto@gmail.com>
2024-07-29 16:27:29 +0100

    added functionality to show dashes for word length

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,5 +1,8 @@
 import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Random;
 import java.io.File;
 
 
@@ -8,9 +11,29 @@ public class Hangman {
 
         Scanner scanner = new Scanner(new File("/Users/jonsmacbookair/Documents/HangmanWords.txt"));
 
+        List<String> words = new ArrayList<>();
+
         while (scanner.hasNext()) {
-            System.out.println(scanner.nextLine());
+           words.add(scanner.nextLine());
+
         }
 
+        Random rand = new Random();
+
+        String word = words.get(rand.nextInt(words.size()));
+        System.out.println(word);
+
+        List<Character> playerGuesses = new ArrayList<>();
+
+        // change this into a method
+        for (int i = 0; i < word.length(); i++) {
+            if (playerGuesses.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+            }
+            else {
+                System.out.print("-");
+            }
+        }
+    System.out.println();
     }
 }
